e0ca17a76d29c0585b4a47588b2f082ed221ebcd
asuender <asuender@student.tgm.ac.at>
2021-11-19 08:06:45 +0100

    Comment ShiftCipher

--- src/asuender/cipher/ShiftCipher.java
+++ src/asuender/cipher/ShiftCipher.java
@@ -1,17 +1,31 @@
 package asuender.cipher;
 
+/**
+ * Diese Klasse verschlüsselt mittels einer Verschiebung der Buchstaben,
+ * welche dann daraus ein (verschobenes) Geheimalphabet ergibt.
+ * @author Andreas Suender
+ * @version 11-19-2021
+ */
 public class ShiftCipher extends MonoAlphabetCipher {
+    /**
+     * Erstellt ein neues ShiftCipher Objekt mit einem Geheimalphabet,
+     * welches um value Buchstaben verschoben ist.
+     * @param value Anzahl der Buchstaben, um welche das Alphabet verschoben werden soll
+     */
     public ShiftCipher(int value) {
         this.setShiftValue(value);
     }
 
+    /**
+     * Erstellt ein neues Geheimalphabet anhand der angegebenen Verschiebung.
+     * @param value Anzahl der Buchstaben, um welche das Alphabet verschoben werden soll
+     */
     public void setShiftValue(int value) {
         String alphabet = "abcdefghijklmnopqrstuvwxyzäöüß";
         StringBuilder secretAlphabet = new StringBuilder();
         for (int i=0; i < alphabet.length(); i++) {
             secretAlphabet.append((char) (alphabet.charAt(i) + value));
         }
-        System.out.println(secretAlphabet);
         super.setSecretAlphabet(secretAlphabet.toString());
     }
 }
